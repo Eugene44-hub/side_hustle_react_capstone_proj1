5028de22a11a52a183cfd8d36c3f39b89bdf6060
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-12-01 12:22:42 +0100

    doc: create error.jsx

---
+++ tour-app/src/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Error = () => {
+   return (
+      <div className="loading">
+         <h3>Something went wrong with the server</h3>
+         <p>Please refresh your browser to try again</p>
+      </div>
+   );
+}
+export default Error;
