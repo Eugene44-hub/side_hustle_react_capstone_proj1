12e8ef6d2f88a31c837fa05fa4cb73733bd6120e
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2021-12-01 12:23:02 +0100

    refactor: udate app.js

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Error from './Error';
+import Loading from './Loading';
 import Tours from './Tours';
 
 function App() {
-  return (
-    <div className='App'>
-      {/* We need to implement a conditional rendering 'Loading....' state based on if the app is loading the data from the API */}
-      <Tours />
-    </div>
-  );
+	const [tours, setTours] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const url = 'https://course-api.com/react-tours-project';
+
+	const fetchTours = async () => {
+		try {
+			setIsLoading(true);
+			const response = await fetch(url);
+			const data = await response.json();
+			setTours(data);
+			setIsLoading(false);
+		} catch (error) {
+			setError(true);
+		}
+	};
+
+	// Runs once when the app loads
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	return (
+		<div className="App">
+			{error && <Error />}
+			{isLoading ? <Loading /> : <Tours tours={tours} />}
+		</div>
+	);
 }
 
 export default App;
